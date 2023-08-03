e99a843f5f76a8b78ae562a6df3d8cf0cfa4df8c
Wallison Kauy <wallisonkauy@gmail.com>
2023-08-03 00:21:26 -0300

    feat: Raio de respawn e respawn de varios inimigos

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -8,22 +6,44 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private GameObject ghrostPrefab;
 
-
     [SerializeField]
     private float ghrostInterval = 3.5f;
-  
+
+    [SerializeField]
+    private float spawnRadius = 30f; // O raio de spawn
+
+    private GameObject player;
+
+    void OnDrawGizmosSelected()
+    {
+        // Desenha um círculo vermelho no editor que representa o raio de spawn
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnEnemy(ghrostInterval, ghrostPrefab));
-       
+        player = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(SpawnEnemy(ghrostInterval, ghrostPrefab));
     }
 
-    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    private IEnumerator SpawnEnemy(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(UnityEngine.Random.Range(-5f, 5), UnityEngine.Random.Range(-6f, 6f), 0), Quaternion.identity);
-        StartCoroutine(spawnEnemy(interval, enemy));
+        Vector3 spawnPosition = RandomPositionWithinRadius(spawnRadius);
+        if (Vector3.Distance(spawnPosition, player.transform.position) <= spawnRadius)
+        {
+            Instantiate(enemy, spawnPosition, Quaternion.identity);
+        }
+        StartCoroutine(SpawnEnemy(interval, enemy));
+    }
+
+    private Vector3 RandomPositionWithinRadius(float radius)
+    {
+        float angle = UnityEngine.Random.Range(0f, 2 * Mathf.PI);
+        float x = radius * Mathf.Cos(angle);
+        float y = radius * Mathf.Sin(angle);
+        return new Vector3(x, y, 0f) + transform.position; // Adicionamos a posição do spawner
     }
 }
