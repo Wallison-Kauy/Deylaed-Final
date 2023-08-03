6c93c4c120c846e2c754bfe8d04646170b4a72a1
LUCAS SILVA IRINEU <lucasirineu@hotmail.com>
2023-08-03 00:43:02 -0300

    Update weapon controller

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -9,9 +9,16 @@ public class WeaponController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle - 180, Vector3.forward);
-        transform.rotation = rotation;
+        if (!PauseMenu.isPaused)
+        {
+            if (!PauseMenu.isPaused)
+            {
+                Vector3 mousePos = Input.mousePosition;
+                Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+                Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+                float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0, 0, angle - 180);
+            }
+        }
     }
 }
